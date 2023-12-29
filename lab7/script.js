81ca1fa48485e10fc72dd2a8281acda6959b99c8
Ankan2810 <22130111@st.hcmuaf.edu.vn>
2023-12-29 22:30:05 +0700

    lab 7

--- lab7/script.js
+++ lab7/script.js
@@ -1,88 +1,32 @@
-// app.js
-document.addEventListener("DOMContentLoaded", function () {
-    const signUpPage = document.getElementById("sign-up");
-
-    // Tạo một trang chính của ứng dụng
-    function createSignUpPage() {
-        signUpPage.innerHTML = `
-        <div class="back"></div>
-        <div class="signUp">
-            <div class="content">
-    
-                <h2>Sign up</h2>
-                <h3>It's quick and easy.</h3>
-    
-                <!-- <div class="icon">
-                    <i class="fa-brands fa-instagram"></i>
-                    <i class="fa-brands fa-facebook"></i>
-                    <i class="fa-brands fa-twitter"></i>
-                    <i class="fa-brands fa-github"></i>
-                </div> -->
-            </div>
-            <form onsubmit="signUp()" >        
-                <div class="home">
-                    <div class="input1">
-                        <input  id="username" type="email" class="input-mail" autocomplete="off" required placeholder="Username">
-                    </div>
-    
-                    <div class="input2">
-                        <input  id="password" type="password" class="input-mail" autocomplete="off" required placeholder="Password">
-                    </div>
-    
-                    <div class="input3">
-                        <input  id="confirm-password" type="password" class="input-mail" autocomplete="off" required placeholder="Re-enter Password">
-                    </div>
-    
-                    <div class="input">
-                        <input type="submit" class="button" value="Sign Up">
-                    </div>
-                    </div>
-            </form>
-    
-        </div>
-        `;
+function makeSnow(){
+    const snow = document.createElement('div');
+    const size = Math.random() *4.5 + 3.5;
+    snow.className = 'snow';
+    snow.style.width = size + 'px';
+    snow.style.height = size + 'px';
+    snow.style.left = Math.random() * window.innerWidth + 'px';
+    snow.style.opacity = size/8;
+    if(size <7){
+        snow.style.zIndex= -5;
     }
-    createSignUpPage();
+    snow.style.animationDuration = Math.random() * 3 + 2 + 's';
+    document.body.appendChild(snow);
+    setTimeout(() => snow.remove(), 5000);
+}
 
-  function signUp(event) {
-        event.preventDefault();
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-        var rePass = document.getElementById("confirm-password").value;
+setInterval(makeSnow, 40);
 
-        if (password !== rePass) {
-            alert("Passwords do not match");
-            return;
-        } else {
-            alert("Sign up successful");
-            window.location.href = "/project/signInPage/index.html";
-        }
-
-        var user = {
-            username: username,
-            password: password,
-        };
-
-        localStorage.setItem(username, JSON.stringify(user));
+function showAndCloseTab(tabName) {
+    // Close all tabs
+    var tabs = document.getElementsByClassName("tab-content");
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
     }
 
-    function signIn(event) {
-        event.preventDefault();
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-
-        var user = localStorage.getItem(username);
-        if (user === null) {
-            alert("Username does not exist");
-            return;
-        }
-
-        var data = JSON.parse(user);
-
-        if (username === data.username && password === data.password) {
-            alert("Login successful");
-        } else {
-            alert("Username or password is incorrect");
-        }
+    // Show the specific tab
+    var tabElement = document.getElementById(tabName);
+    if (tabElement) {
+        tabElement.style.display = "block";
     }
-})
+}
+
